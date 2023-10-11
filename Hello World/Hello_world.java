9610c65317e027b8f804fa9257dde05cfd1a0553
MertensFlo <fmertens20@gmail.com>
2023-10-11 19:00:09 +0200

    Added first Test Hello_world

---
+++ Hello World/Hello_world.java
@@ -0,0 +1,7 @@
+public class Hello_world(){
+
+    public static void main(void args){
+        System.out.println("Hello World");
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # blockchain_test
 Studienseminar OTH Regenburg Kurs Blockchain
+
+Repository for the Blockchain lecture
